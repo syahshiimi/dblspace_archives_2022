e6b2aa899da212329a3b2bfa262e3e1944897df4
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-11-12 20:20:43 +0800

    add useStates

--- components/floatingpills.tsx
+++ components/floatingpills.tsx
@@ -3,10 +3,12 @@ import { ReactNode } from "react";
 type FloatingPillsProps = {
     pillColor: string;
     children: ReactNode;
+    setIsOpen: any;
+    isOpen: any;
 }
-const FloatingPills = ({ pillColor, children }: FloatingPillsProps) => {
+const FloatingPills = ({ pillColor, children, setIsOpen }: FloatingPillsProps) => {
     return (
-        <div className={`flex flex-row ${pillColor} w-fit px-4 py-5 rounded-xl`} >
+        <div className={`flex flex-row ${pillColor} w-fit px-4 py-5 rounded-xl`} onClick={() => setIsOpen(true)} >
             <p className="text-black text-xl font-semibold leading-[0px]">
                 {children}
             </p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import { useState, Fragment } from 'react'
 import Head from 'next/head'
 import FloatingPills from '../components/floatingpills'
+import { Dialog, Transition } from '@headlessui/react'
 
 export default function Home() {
+    let [isOpen, setIsOpen] = useState(true)
+
     return (
         <>
             <Head>
@@ -9,21 +13,58 @@ export default function Home() {
                 <meta name="description" content="SAJ" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <Transition
+                show={isOpen}
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-75 opacity-0"
+                as={Fragment}
+            >
+                <Dialog
+                    open={isOpen}
+                    onClose={() => setIsOpen(false)}
+                    className="z-50 inset-0 fixed flex justify-center items-center "
+                >
+                    <div className="fixed inset-0 bg-black/70 " aria-hidden="true" />
+                    <Dialog.Panel className="z-10 bg-mikado-yellow px-6 py-4 rounded-xl">
+                        <Dialog.Title
+                            className={"text-2xl font-bold text-black"}>Deactivate account</Dialog.Title>
+
+                        <Dialog.Description>
+                            This will permanently deactivate your account
+                        </Dialog.Description>
+
+                        <p>
+                            Are you sure you want to deactivate your account? All of your data
+                            will be permanently removed. This action cannot be undone.
+                        </p>
+
+                        <button onClick={() => setIsOpen(false)}>Deactivate</button>
+                        <button onClick={() => setIsOpen(false)}>Cancel</button>
+                    </Dialog.Panel>
+
+                </Dialog>
+            </Transition>
+
+
             <div className="flex flex-col items-center justify-center grow">
                 <div className='container mx-auto flex flex-col gap-4 items-center justify-center '>
-                    <FloatingPills pillColor='bg-mikado-yellow gap-4'>
+                    <FloatingPills pillColor="bg-mikado-yellow gap-4" setIsOpen={setIsOpen} isOpen={isOpen}>
                         Green Screen Machine
                     </FloatingPills>
-                    <FloatingPills pillColor='bg-brilliant-azure'>
+                    <FloatingPills pillColor="bg-brilliant-azure">
                         Field Recordings
                     </FloatingPills>
-                    <FloatingPills pillColor='bg-willpower-orange'>
+                    <FloatingPills pillColor="bg-willpower-orange">
                         Space Activations
                     </FloatingPills>
-                    <FloatingPills pillColor='bg-vivid-raspberry' >
+                    <FloatingPills pillColor="bg-vivid-raspberry" >
                         Objects On A Wall
                     </FloatingPills>
-                    <FloatingPills pillColor='bg-blue-violet' >
+                    <FloatingPills pillColor="bg-blue-violet" >
                         Totz n Feelingz Wall
                     </FloatingPills>
                 </div>
