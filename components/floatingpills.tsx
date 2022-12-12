e32dbe5cf37ce6eb1edb262889e726e76fc19ed1
syahshiimi <syahrlanuar95@gmail.com>
2022-12-13 00:19:02 +0800

    improve component interface types

--- components/floatingpills.tsx
+++ components/floatingpills.tsx
@@ -1,23 +1,25 @@
-import { Dispatch, ReactNode, SetStateAction } from 'react'
+import { Dispatch, ReactNode, SetStateAction } from "react"
 
 interface IFLoatingPill {
   pillColor:
-    | 'bg-mikado-yellow'
-    | 'bg-brilliant-azure'
-    | 'bg-willpower-orange'
-    | 'bg-vivid-raspberry'
-    | 'bg-blue-violet'
+    | "bg-mikado-yellow"
+    | "bg-brilliant-azure"
+    | "bg-willpower-orange"
+    | "bg-vivid-raspberry"
+    | "bg-blue-violet"
+    | string
     | undefined
-  children: ReactNode | undefined
-  setIsOpen: Dispatch<SetStateAction<boolean>>
-  isOpen: boolean | undefined
+  children?: ReactNode | undefined
+  setIsOpen?: Dispatch<SetStateAction<boolean>> | undefined
 }
 const FloatingPills = ({ pillColor, children, setIsOpen }: IFLoatingPill) => {
   return (
     <button
       type="button"
       className={`flex flex-row ${pillColor} w-fit rounded-xl px-4 py-6 transition-all ease-in-out hover:-translate-y-2`}
-      onClick={() => setIsOpen(true)}
+      onClick={() => {
+        setIsOpen != undefined ? setIsOpen(true) : null
+      }}
     >
       <p className="text-xl font-semibold leading-[0px] text-black">
         {children}
