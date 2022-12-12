82e9036f94a689c2880c9da31343d035e6dd4c89
syahshiimi <syahrlanuar95@gmail.com>
2022-12-13 00:19:14 +0800

    add collect button hyperlink

--- components/navigation/footer.tsx
+++ components/navigation/footer.tsx
@@ -1,10 +1,19 @@
-const Footer = () => {
-    return (
-        <footer className="flex flex-row font-thin italic text-sm">
-            Inspired by the floating DVD and in no way related to it.
-        </footer>
+import Link from "next/link"
+import FloatingPills from "../floatingpills"
 
-    )
+const Footer = () => {
+  return (
+    <footer className="flex flex-row items-end justify-between">
+      <div>
+        <p className="text-sm font-thin italic">
+          Inspired by the floating DVD and in no way related to it.
+        </p>
+      </div>
+      <FloatingPills pillColor="bg-brilliant-azure">
+        <Link href={"/collect"}>COLLECT</Link>
+      </FloatingPills>
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
